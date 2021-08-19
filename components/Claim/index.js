589e13792aa8d4994b21a56d5b3aed6c0e0ee625
Raihan Iyai <raihaniyai@gmail.com>
2021-08-19 10:44:09 +0700

    remove ads

--- components/Claim/index.js
+++ components/Claim/index.js
@@ -21,24 +21,6 @@ const ClaimComponent = ({ data, handleClaim, isEligible }) => {
           />
         </Col>
       </Row>
-
-      <div className={Advertisement}>
-        <script type="text/javascript">
-          var pubId=157566;
-          var siteId=851482;
-          var kadId=3932573;
-          var kadwidth=300;
-          var kadheight=250;
-          var kadschain="SUPPLYCHAIN_GOES_HERE";
-          var kadUsPrivacy=""; var kadtype=1;
-          var kadGdpr="0";
-          var kadGdprConsent=""; var kadexpdir = '1,2,3,4,5';
-          var kadbattr = '8,9,10,11,14';
-          var kadifb = 'Dc';
-          var kadpageurl= "https%3A%2F%2Fgitaran.vercel.app%2F";
-        </script>
-        <script type="text/javascript" src="https://ads.pubmatic.com/AdServer/js/showad.js"></script>
-      </div>
     </>
   );
 };
